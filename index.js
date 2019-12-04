e162f400ef61699f40d65cc4555898ca32433d6a
johnny zhu <johnny.zhu26@gmail.com>
2019-12-04 16:50:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var arr = [] 
     for(i = 0; i < musicians.length; i++){
-      
+      var string = musicians[i] + instruments[i]
+      arr
     }
 }
